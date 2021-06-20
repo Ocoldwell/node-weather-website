3c7a4a2a91b79c594616820f0ac0a4251f08ce26
Oliver Coldwell <olivercoldwell@gmail.com>
2021-06-20 16:15:09 +0100

    added some comments to refer back to

--- src/app.js
+++ src/app.js
@@ -2,11 +2,15 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
+// Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public');
 const viewsPath = path.join(__dirname, '../templates');
 
+// Setup handlebars engine and views location
 app.set('view engine', 'hbs');
 app.set('views', viewsPath);
+
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
 
 app.get('', (req, res) => {
