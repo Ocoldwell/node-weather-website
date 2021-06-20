b9db4ac28a13c5e01911842c619f793c32798490
Oliver Coldwell <olivercoldwell@gmail.com>
2021-06-20 16:13:48 +0100

    added dynamic pages using templates and handlebars

--- src/app.js
+++ src/app.js
@@ -1,21 +1,39 @@
+const path = require('path');
 const express = require('express');
 
 const app = express();
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates');
+
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+app.use(express.static(publicDirectoryPath));
 
 app.get('', (req, res) => {
-  res.send('Hello express!');
-})
-
-app.get('/help', (req, res) => {
-  res.send('Help page');
+  res.render('index', {
+    title: 'Weather',
+    name: 'Oliver Coldwell'
+  })
 })
 
 app.get('/about', (req, res) => {
-  res.send('About page');
+  res.render('about', {
+    title: 'About Me',
+    name: 'Oliver Coldwell'
+  })
+})
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    message: "Help I'm an example message, get me out of here"
+  })
 })
 
 app.get('/weather', (req,res) => {
-  res.send('Weather page');
+  res.send({
+    forecast: 'Here I am words',
+    location: 'hello location location location'
+  });
 })
 
 app.listen(3000, () => {
