5badcfe53915a59c3cf0ab194b0d8ef33f5a8d70
Oliver Coldwell <olivercoldwell@gmail.com>
2021-06-21 02:03:02 +0100

    Setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -6,7 +6,7 @@ const messageTwo = document.querySelector("#message-two");
 const fetchWeather = search => {
   messageOne.textContent = 'Loading...';
   messageTwo.textContent = '';
-  fetch(`http://localhost:3000/weather?address=${search}`).then((response) => {
+  fetch(`/weather?address=${search}`).then((response) => {
     response.json().then((data) => {
       if (data.error) {
         messageOne.textContent= data.error
