83aff0ace3ad61f6cf580d7f36f30530a4a86d23
Oliver Coldwell <olivercoldwell@gmail.com>
2021-06-21 01:10:32 +0100

    added html rendering of weather api calls

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,28 @@
-console.log('client side javascript file is loaded')
+console.log("client side javascript file is loaded");
+
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+const messageOne = document.querySelector("#message-one");
+const messageTwo = document.querySelector("#message-two");
+
+const fetchWeather = search => {
+  messageOne.textContent = 'Loading...';
+  messageTwo.textContent = '';
+  fetch(`http://localhost:3000/weather?address=${search}`).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        messageOne.textContent= data.error
+      } else {
+        messageOne.textContent= data.location;
+        messageTwo.textContent= data.forecast;
+      }
+    });
+  });
+};
+
+
+weatherForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const location = search.value;
+  fetchWeather(location);
+});
